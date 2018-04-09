b8c217375e06fef81450a0ff58bd2cbd8f4c8b69
oscar ortiz <ortizg.oscar@gmail.com>
2018-04-09 16:37:25 -0300

    Associations Done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :visited_steplists
+  has_many :steplists
+  has_many :steplists, through: :visited_steplists
+
+
 end
