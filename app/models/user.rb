9aceba402187702207829624f0cae42214627573
oscar ortiz <ortizg.oscar@gmail.com>
2018-04-11 19:33:58 -0300

    pause

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-
   after_create :send_welcome_email
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :steplists
   has_many :visualizations
-  has_many :visualized_steplists, through: :visualizations, source: :steplist
+  has_many :visualized_steps, through: :visualizations, source: :step
   validates :first_name, :last_name, presence: true
 
   private
