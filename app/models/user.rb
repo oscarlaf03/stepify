b272dfc56faeb514a134629b64e2765842655d46
oscar ortiz <ortizg.oscar@gmail.com>
2018-04-12 10:48:15 -0300

    Done fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :steplists
+  has_many :steps, through: :steplists
   has_many :visualizations
   has_many :visualized_steps, through: :visualizations, source: :step
   validates :first_name, :last_name, presence: true
