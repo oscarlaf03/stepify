088c38b5fb239d26feea0198336e8204a83a0e74
Miguel Aguirre <md.aguim@gmail.com>
2018-04-17 10:24:28 -0300

    Removed extra div

--- db/schema.rb
+++ db/schema.rb
@@ -15,20 +15,6 @@ ActiveRecord::Schema.define(version: 20180416180045) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.string "author_type"
-    t.bigint "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
-  end
-
   create_table "organization_users", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "organization_id"
@@ -108,12 +94,6 @@ ActiveRecord::Schema.define(version: 20180416180045) do
     t.string "first_name"
     t.string "last_name"
     t.string "user_photo"
-    t.string "provider"
-    t.string "uid"
-    t.string "facebook_picture_url"
-    t.string "token"
-    t.datetime "token_expiry"
-    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
