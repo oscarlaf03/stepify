e76680bf4e05baaab33d4a8f676e79ae78db4762
oscar ortiz <ortizg.oscar@gmail.com>
2018-04-17 21:31:54 -0300

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, only: [:dashboard, :new, :edit, :my_lists]
+  before_action :authenticate_user!, only: [:dashboard, :new, :edit, :my_lists, :my_pins]
   include Pundit
   after_action :verify_authorized, except: [:index, :show, :home, :dashboard], unless: :skip_pundit?
   after_action :verify_policy_scoped, only: [:dashboard], unless: :skip_pundit?
