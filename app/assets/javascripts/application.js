62be9314e1296093f9ad3808ac43af5c0e6bb376
oscar ortiz <ortizg.oscar@gmail.com>
2018-04-19 15:14:17 -0300

    step counter working on modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,9 +2,3 @@
 //= require jquery_ujs
 //= require best_in_place
 //= require_tree .
-
-var steps = document.querySelectorAll('.step-callback');
-
-for (i = 0; i < steps.length; i++) {
-  console.log(steps[i])
-}
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,16 @@
 import "bootstrap";
+
+
+var steps = document.querySelectorAll('.step-callback');
+var i;
+for (i = 0; i < steps.length; i++) {
+  $(steps[i]).on('shown.bs.modal', function (e) {
+    var id = this.id.split("_")[1]
+    $.get({
+      url: "/steps/" + id,
+      success: function(result){
+        console.log("oi")
+      }
+    });
+  })
+}
