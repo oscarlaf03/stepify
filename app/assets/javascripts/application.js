b2d1f1fe1abe7c6aed21ceb3dd59fb717abc6af4
oscar ortiz <ortizg.oscar@gmail.com>
2018-04-19 19:01:35 -0300

    Fixed links on card

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,8 @@
 //= require jquery_ujs
 //= require best_in_place
 //= require_tree .
+
+// $('.steplist-card-body').on('click', function() {
+//   var id = $(this).data("link");
+//   window.location = "/steplists/" + id;
+// });
